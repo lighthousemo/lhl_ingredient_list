c81856ac35a6a2c238c94b2878892e516393d79c
Monica Olinescu <monica.olinescu@gmail.com>
2016-06-30 15:25:47 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+
