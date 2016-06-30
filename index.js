95d3033b910427e516f0a3997506d6863c5da36e
Monica Olinescu <monica.olinescu@gmail.com>
2016-06-30 15:55:45 -0700

    Prepare recipes for ejs file

--- index.js
+++ index.js
@@ -7,5 +7,34 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+var database = {
+  ingredients: [
+    {id: 1, name: "Apple Juice"},
+    {id: 2, name: "Orange Juice"},
+    {id: 3, name: "Kale"},
+    {id: 4, name: "Spinach"},
+    {id: 5, name: "Mango"},
+  ],
+  recipes: [
+    {title: "Mighty Kale", image_url: "http://prod-jugojuice.s3.amazonaws.com/products/17/en:/original/Mighty-Kale-flat.jpg", ingredients: [1, 2, 3, 4]},
+    {title: "Spinache Magic", image_url: "http://i1.wp.com/yesiamvegan.com/wp-content/uploads/2015/01/DSC_00072.jpg", ingredients: [4, 1, 5]},
+  ]
+}
 
+app.get("/recipes", (req, res) => {
+  // prep data to send down to the template
+  const recipeBox = database.recipes.map((recipe) => {
+    const newIngredients = recipe.ingredients.map((ingredientID) => {
+      return getIngredient(ingredientID).name;
+    }); // ["Apple Juice", "Orange Juice", ...]
+    return {title: recipe.title, image_url: recipe.image_url, ingredients: newIngredients};
+  });
+  res.render("recipes", {recipeBox: recipeBox});
+});
+
+function getIngredient(id) {
+  return database.ingredients.find((ingredient) => {
+    return ingredient.id === ingredientID;
+  }); // ex: {id: 1, name: "Apple Juice"}
+}
 
